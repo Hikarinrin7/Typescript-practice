e678fd9c6b51e782949e5201866112d5c366abf2
Ohta Akari <shizuokaocha88@gmail.com>
2025-01-26 15:55:11 +0900

    sec4

---
+++ sec4-1.ts
@@ -0,0 +1,18 @@
+
+
+const add = (n1, n2) => {
+  if (typeof n1 !== 'number'|| typeof n2 !== 'number'){
+    throw new Error('n1またはn2がnumberではないのでエラーです')
+  }
+  return n1+n2
+}
+
+let num1 ="1";
+let num2 =2;
+
+const result = add(num1,num2);
+
+console.log(result);
+
+// 文字列と数字の和は文字列として扱われる
+// typeofを使って型チェックができる
